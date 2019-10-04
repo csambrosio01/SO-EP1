755035e851fb5d9f50e6db4aa0f623c2078d7f65
Caio Koiti Nakazawa <koiti2000@hotmail.com>
2019-10-04 19:22:56 -0300

    moving PCB class

---
+++ src/scheduler/PCB.java
@@ -0,0 +1,87 @@
+package scheduler;
+
+public class PCB implements Comparable<PCB>{
+	private Process process;
+	private final int priority;
+	private int programCounter;
+	private int registerX;
+	private int registerY;
+	private int credit;
+	private int wait;
+	
+
+	public PCB(Process process, int priority) {
+		this.process = process;
+		this.programCounter = 0;
+		this.priority = priority;
+		this.credit = priority;
+		this.registerX = 0;
+		this.registerY = 0;
+		this.wait = 0;
+	}
+	
+	// Process methods
+	public Process getProcess() {
+		return this.process;
+	}
+	
+	// PC methods
+	public void increaseProgramCounter() {
+		this.programCounter++;
+	}
+	
+	public int getProgramCounter() {
+		return this.programCounter;
+	}
+	
+	// Credit methods
+	public void decreaseTwoCredits() {
+		this.credit = (this.credit <= 2) ? 0 : this.credit - 2;
+	}
+	
+	public void equalsCreditWithPriority() {
+		this.credit = this.priority;
+	}
+	
+	public int getCredit() {
+		return this.credit;
+	}
+	
+	//Register methods
+	public int getRegisterX() {
+		return this.registerX;
+	}
+	
+	public void setRegisterX(int x) {
+		this.registerX = x;
+	}
+	
+	public int getRegisterY() {
+		return this.registerY;
+	}
+	
+	public void setRegisterY(int y) {
+		this.registerY = y;
+	}
+	
+	// Wait methods
+	public void decreaseWait() {
+		this.wait--;
+	}
+	
+	public void setWaitTo2() {
+		this.wait = 2;
+	}
+	
+	public int getWait() {
+		return this.wait;
+	}
+	
+
+	@Override
+	public int compareTo(PCB pcb) {
+		if (this.credit < pcb.credit) return 1;
+		else if (this.credit > pcb.credit) return -1;
+		else return 0;
+	}
+}
