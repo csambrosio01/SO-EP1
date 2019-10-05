a64639bde24cebf63daf04a03ce73382e156ff2f
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-05 15:03:49 -0300

    Create constructor for ReadDatas

--- src/ioDatas/ReadDatas.java
+++ src/ioDatas/ReadDatas.java
@@ -1,5 +1,13 @@
 package ioDatas;
 
+import java.io.IOException;
+
 public class ReadDatas {
+
+    private Log logger;
+
+    public ReadDatas() throws IOException {
+        logger = Log.getInstance();
+    }
 
 }
