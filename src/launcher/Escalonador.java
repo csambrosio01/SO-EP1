b72ee8d254ef4b88a9113cb8816cef46a725e216
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-05 14:58:44 -0300

    Add function to read quantum from quantum.txt and set static param quantum of main class with readed value

--- src/launcher/Escalonador.java
+++ src/launcher/Escalonador.java
@@ -1,9 +1,20 @@
 package launcher;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Escalonador {
 
-	public static void main(String[] args) {
+	public static int quantum = 3; //Set default quantum as 3
 
+	private static void readQuantum() throws IOException {
+		Scanner scanner = new Scanner(new File("src/processos/quantum.txt"));
+		quantum = scanner.nextInt();
+	}
+
+	public static void main(String[] args) throws IOException {
+		readQuantum();
 	}
 
 }
