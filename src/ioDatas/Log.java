c662f4fc92148d92e18a7dfc5283ab8bbeada807
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-05 15:00:10 -0300

    Refact Log constructor to use static value quantum of main class

--- src/ioDatas/Log.java
+++ src/ioDatas/Log.java
@@ -1,12 +1,12 @@
 package ioDatas;
 
-import java.io.File;
+import launcher.Escalonador;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 public class Log {
 
@@ -19,8 +19,7 @@ public class Log {
 		Locale ptBR = new Locale("pt","BR");
 		this.bundle = ResourceBundle.getBundle("messages", ptBR);
 
-		Scanner scanner = new Scanner(new File("src/processos/quantum.txt"));
-		int quantum = scanner.nextInt();
+		int quantum = Escalonador.quantum;
 
 		String filename = quantum < 10 ? ("log0" +quantum+ ".txt") : ("log" +quantum+ ".txt");
 		logFile = new PrintStream("src/processos/" +filename);
