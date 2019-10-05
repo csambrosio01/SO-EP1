0248012eaa377e83b816a446c5451d6f1cc83b21
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-05 15:08:48 -0300

    Create function to read file based on its number, and return equivalente PCB

--- src/ioDatas/ReadDatas.java
+++ src/ioDatas/ReadDatas.java
@@ -1,6 +1,14 @@
 package ioDatas;
 
+import scheduler.PCB;
+import scheduler.Process;
+import scheduler.State;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadDatas {
 
@@ -8,6 +16,24 @@ public class ReadDatas {
 
     public ReadDatas() throws IOException {
         logger = Log.getInstance();
+    }
+
+    public PCB readFile(int proccessNumber, int processPriority) throws Exception {
+        String processPath = "src/processos/" + (proccessNumber < 10 ? "0" + proccessNumber : proccessNumber) + ".txt";
+        BufferedReader reader = new BufferedReader(new FileReader(processPath));
+
+        List<String> instructions = new ArrayList<>();
+
+        String fileName = reader.readLine();
+        logger.addMessage("LOADING_PROCESS", fileName);
+
+        String content;
+        while ((content = reader.readLine()) != null) {
+            instructions.add(content);
+        }
+
+        Process process = new Process(fileName, instructions.toArray(new String[0]), State.READY);
+        return new PCB(process, processPriority);
     }
 
 }
