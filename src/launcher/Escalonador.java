4c9072d644d3e0e2122eb9a9db6254af731b8652
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-05 15:02:01 -0300

    Add function to read each process priority and return it as a List of Integers

--- src/launcher/Escalonador.java
+++ src/launcher/Escalonador.java
@@ -1,8 +1,14 @@
 package launcher;
 
 import ioDatas.Log;
+import scheduler.PCB;
+import scheduler.ProcessList;
+import scheduler.ProcessTable;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Escalonador {
@@ -14,6 +20,19 @@ public class Escalonador {
 	private static void readQuantum() throws IOException {
 		Scanner scanner = new Scanner(new File("src/processos/quantum.txt"));
 		quantum = scanner.nextInt();
+	}
+
+	private static List<Integer> readPriorities() throws IOException {
+		List<Integer> priorities = new ArrayList<>();
+
+		Scanner prioritiesFile = new Scanner(new File("src/processos/prioridades.txt"));
+
+		while (prioritiesFile.hasNext()) {
+			String priority = prioritiesFile.next();
+			priorities.add(Integer.parseInt(priority));
+		}
+
+		return priorities;
 	}
 
 	public static void main(String[] args) throws IOException {
