49747f28be718f45d94bfc91855940269b99365c
Caio Ambrosio <caio.ambrosio@braspay.co>
2019-10-06 14:14:28 -0300

    Create constructor for Scheduler class

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,5 +1,13 @@
 package scheduler;
 
+import ioDatas.Log;
+import java.io.IOException;
+
 public class Scheduler {
 
+    private Log logger;
+
+    public Scheduler() throws IOException {
+        logger = Log.getInstance();
+    }
 }
