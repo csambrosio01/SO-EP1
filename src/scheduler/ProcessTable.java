f4dae454675144cbb6a8777cbb7cece8a39d2eb8
Caio Koiti Nakazawa <koiti2000@hotmail.com>
2019-10-26 02:30:33 -0300

    add comments in ProcessTable class

--- src/scheduler/ProcessTable.java
+++ src/scheduler/ProcessTable.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessTable {
+	// List with all PCBs
 	public static List<PCB> processTable = new ArrayList<PCB>();
-	
+
+	// Add PCB on processTable
 	public static void addPCBonProcessTable(PCB pcb) {
 		processTable.add(pcb);
 	}
-	
+
+	// Remove PCB of processTable
 	public static void removePCBofProcessTable(PCB pcb) {
 		processTable.remove(pcb);
 	}
